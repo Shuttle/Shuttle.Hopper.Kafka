95397ad2ea5e08e2aa4b17fc7aefbc514a612895
Eben <me@ebenroux.co.za>
2025-12-14 13:27:36 +0200

    ServiceCollectionExtensions refactored to new format.

--- Shuttle.Hopper.Kafka/ServiceCollectionExtensions.cs
+++ Shuttle.Hopper.Kafka/ServiceCollectionExtensions.cs
@@ -7,55 +7,58 @@ namespace Shuttle.Hopper.Kafka;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddKafka(this IServiceCollection services, Action<KafkaBuilder>? builder = null)
+    extension(IServiceCollection services)
     {
-        var kafkaBuilder = new KafkaBuilder(Guard.AgainstNull(services));
-
-        builder?.Invoke(kafkaBuilder);
-
-        services.AddSingleton<IValidateOptions<KafkaOptions>, KafkaOptionsValidator>();
-
-        foreach (var pair in kafkaBuilder.KafkaOptions)
+        public IServiceCollection AddKafka(Action<KafkaBuilder>? builder = null)
         {
-            services.AddOptions<KafkaOptions>(pair.Key).Configure(options =>
+            var kafkaBuilder = new KafkaBuilder(Guard.AgainstNull(services));
+
+            builder?.Invoke(kafkaBuilder);
+
+            services.AddSingleton<IValidateOptions<KafkaOptions>, KafkaOptionsValidator>();
+
+            foreach (var pair in kafkaBuilder.KafkaOptions)
             {
-                options.ConsumerBuilder = pair.Value.ConsumerBuilder;
-                options.ProducerBuilder = pair.Value.ProducerBuilder;
-                options.ConsumerConfig = pair.Value.ConsumerConfig;
-                options.ProducerConfig = pair.Value.ProducerConfig;
-
-                options.BootstrapServers = pair.Value.BootstrapServers;
-                options.MessageSendMaxRetries = pair.Value.MessageSendMaxRetries;
-                options.NumPartitions = pair.Value.NumPartitions;
-                options.ReplicationFactor = pair.Value.ReplicationFactor;
-                options.RetryBackoff = pair.Value.RetryBackoff;
-                options.EnableAutoCommit = pair.Value.EnableAutoCommit;
-                options.EnableAutoOffsetStore = pair.Value.EnableAutoOffsetStore;
-                options.FlushEnqueue = pair.Value.FlushEnqueue;
-                options.UseCancellationToken = pair.Value.UseCancellationToken;
-                options.ConsumeTimeout = pair.Value.ConsumeTimeout;
-                options.ConnectionsMaxIdle = pair.Value.ConnectionsMaxIdle;
-                options.OperationTimeout = pair.Value.OperationTimeout;
-
-                if (options.ConsumeTimeout < TimeSpan.FromMilliseconds(25))
+                services.AddOptions<KafkaOptions>(pair.Key).Configure(options =>
                 {
-                    options.ConsumeTimeout = TimeSpan.FromMilliseconds(25);
-                }
+                    options.ConsumerBuilder = pair.Value.ConsumerBuilder;
+                    options.ProducerBuilder = pair.Value.ProducerBuilder;
+                    options.ConsumerConfig = pair.Value.ConsumerConfig;
+                    options.ProducerConfig = pair.Value.ProducerConfig;
 
-                if (options.ConnectionsMaxIdle < TimeSpan.Zero)
-                {
-                    options.ConnectionsMaxIdle = TimeSpan.Zero;
-                }
+                    options.BootstrapServers = pair.Value.BootstrapServers;
+                    options.MessageSendMaxRetries = pair.Value.MessageSendMaxRetries;
+                    options.NumPartitions = pair.Value.NumPartitions;
+                    options.ReplicationFactor = pair.Value.ReplicationFactor;
+                    options.RetryBackoff = pair.Value.RetryBackoff;
+                    options.EnableAutoCommit = pair.Value.EnableAutoCommit;
+                    options.EnableAutoOffsetStore = pair.Value.EnableAutoOffsetStore;
+                    options.FlushEnqueue = pair.Value.FlushEnqueue;
+                    options.UseCancellationToken = pair.Value.UseCancellationToken;
+                    options.ConsumeTimeout = pair.Value.ConsumeTimeout;
+                    options.ConnectionsMaxIdle = pair.Value.ConnectionsMaxIdle;
+                    options.OperationTimeout = pair.Value.OperationTimeout;
 
-                if (options.OperationTimeout < TimeSpan.FromMilliseconds(25))
-                {
-                    options.OperationTimeout = TimeSpan.FromMilliseconds(25);
-                }
-            });
+                    if (options.ConsumeTimeout < TimeSpan.FromMilliseconds(25))
+                    {
+                        options.ConsumeTimeout = TimeSpan.FromMilliseconds(25);
+                    }
+
+                    if (options.ConnectionsMaxIdle < TimeSpan.Zero)
+                    {
+                        options.ConnectionsMaxIdle = TimeSpan.Zero;
+                    }
+
+                    if (options.OperationTimeout < TimeSpan.FromMilliseconds(25))
+                    {
+                        options.OperationTimeout = TimeSpan.FromMilliseconds(25);
+                    }
+                });
+            }
+
+            services.TryAddSingleton<ITransportFactory, KafkaStreamFactory>();
+
+            return services;
         }
-
-        services.TryAddSingleton<ITransportFactory, KafkaStreamFactory>();
-
-        return services;
     }
 }
